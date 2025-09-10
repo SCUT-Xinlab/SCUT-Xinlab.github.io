60b5b4a9ca41dc1d32165d474cac4e09a261d855
WangCheng-13 <2500846212@qq.com>
2025-09-10 18:00:57 +0800

    update: 2025-09-10 18:00:56

---
+++ auto_push.py
@@ -0,0 +1,12 @@
+# 在当前 根目录 下创建auto_push.py
+import subprocess
+from datetime import datetime
+
+def auto_push():
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    msg = f"update: {now}"
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", msg], check=True)
+    subprocess.run(["git", "push", "origin", "main"], check=True)
+
+auto_push()
